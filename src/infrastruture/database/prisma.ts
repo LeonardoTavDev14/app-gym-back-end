77695dcf3b4218bc840d188a247c13720801db1c
LeonardoTavDev14 <contatoleonardotavdev@gmail.com>
2025-12-10 20:58:10 -0300

    feat: fixing build deploy

--- src/infrastruture/database/prisma.ts
+++ src/infrastruture/database/prisma.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 dotenv.config();
+import { PrismaClient } from "../../../node_modules/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "../../generated/prisma/client";
+
 const connectionString = `${process.env.DATABASE_URL}`;
 
 const adapter = new PrismaPg({ connectionString });
